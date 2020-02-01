af8518a4b59ef400de18b2fe39676a832501dd0e
Vadim Zeitlin <vadim@wxwidgets.org>
2020-02-01 01:57:04 +0100

    Simplify check for current cell in Home/End handling code
    Prefer to just test whether we have it instead of breaking out of the
    case if we don't, this is slightly more straightforward.
    
    No real changes.

--- src/generic/grid.cpp
+++ src/generic/grid.cpp
@@ -5475,10 +5475,8 @@ void wxGrid::OnKeyDown( wxKeyEvent& event )
                 break;
 
             case WXK_HOME:
+                if ( m_currentCellCoords != wxGridNoCellCoords )
                 {
-                    if ( m_currentCellCoords == wxGridNoCellCoords )
-                        break;
-
                     const bool useSelectedBlockCorner =
                         event.ShiftDown() && m_selectedBlockCorner != wxGridNoCellCoords;
                     int row = useSelectedBlockCorner ? m_selectedBlockCorner.GetRow()
@@ -5518,10 +5516,8 @@ void wxGrid::OnKeyDown( wxKeyEvent& event )
                 break;
 
             case WXK_END:
+                if ( m_currentCellCoords != wxGridNoCellCoords )
                 {
-                    if ( m_currentCellCoords == wxGridNoCellCoords )
-                        break;
-
                     const bool useSelectedBlockCorner =
                         event.ShiftDown() && m_selectedBlockCorner != wxGridNoCellCoords;
                     int row = useSelectedBlockCorner ? m_selectedBlockCorner.GetRow()
