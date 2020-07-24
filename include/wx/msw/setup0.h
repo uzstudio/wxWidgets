8284f68298344dd76db86e4dbac4376b642afdfe
Leland Lucius <github@homerow.net>
2020-07-24 10:53:29 -0500

    Remove patch to msw/setup0.h
    This will restore default wxDEBUG_LEVEL handling on Windows.

--- include/wx/msw/setup0.h
+++ include/wx/msw/setup0.h
@@ -87,12 +87,11 @@
 // NDEBUG) or, on the contrary, enable more asserts, including the usually
 // disabled ones, in the debug build (then do it inside #ifndef NDEBUG)
 //
-// Uncommented for Audacity, and set to 1 (rather than 2) for debug builds.
-#ifdef NDEBUG
-  #define wxDEBUG_LEVEL 0
-#else
-  #define wxDEBUG_LEVEL 1
-#endif
+// #ifdef NDEBUG
+//  #define wxDEBUG_LEVEL 0
+// #else
+//  #define wxDEBUG_LEVEL 2
+// #endif
 
 // wxHandleFatalExceptions() may be used to catch the program faults at run
 // time and, instead of terminating the program with a usual GPF message box,
