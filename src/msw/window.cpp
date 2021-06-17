e42d7f73652cf7e40058a48177a86e0f864ec464
Dmitry Vedenko <vedenko@gmail.com>
2021-06-17 15:11:43 +0300

    Fix accsessibility for 64bit Windows applications

--- src/msw/window.cpp
+++ src/msw/window.cpp
@@ -3559,10 +3559,14 @@ wxWindowMSW::MSWHandleMessage(WXLRESULT *result,
 #if wxUSE_ACCESSIBILITY
         case WM_GETOBJECT:
             {
-                //WPARAM dwFlags = (WPARAM) (DWORD) wParam;
-                LPARAM dwObjId = (LPARAM) (DWORD) lParam;
-
-                if (dwObjId == (LPARAM)OBJID_CLIENT && GetOrCreateAccessible())
+                // As per MSDN example - the correct way to compare this values
+                // is to cast them both to DWORD
+                // https://docs.microsoft.com/en-us/windows/win32/winauto/reuse-existing-pointers-to-objects
+                // For the lParam - sign extension happens, as LPARM is a signed integer.
+                // OBJID_CLIENT is defined as ((LONG)0xFFFFFFFC), we cast it DWORD so no warning is generated
+                // In the original code, however, it was sign extended as well, as we were comparing againg signed
+                // 64-bit value.
+                if ((DWORD)lParam == (DWORD)OBJID_CLIENT && GetOrCreateAccessible())
                 {
                     processed = true;
                     rc.result = LresultFromObject(IID_IAccessible, wParam, (IUnknown*) GetAccessible()->GetIAccessible());
